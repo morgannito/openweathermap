3676f3eca8698fa0d94b135aa137965ed4152abc
morga <mriu.morgannito@gmail.com>
2021-02-16 10:46:21 +0100

    update

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import requests
 attitude: float = 42.6886591
 longitude: float = 2.8948332
 api_key = "b8bdb7d52b20aa7c239f409d7f009840"
-
+global tabCity
+tabCity = ""
 
 # todo refaire avec inpute longitute lattitude pui faire un forecast sur 5 jours pour toute les villes
 # todo enregistre dans un csv avec 3 colonnes
@@ -20,7 +21,7 @@ def copie(tab):
 
 
 def circle(att, long, key):
-    cnt: int = 50
+    cnt: int = 5
     # requete post
     url = "https://api.openweathermap.org/data/2.5/find?lat=%s&lon=%s&cnt=%s&appid=%s" % (
         att, long, cnt, key)
@@ -28,35 +29,36 @@ def circle(att, long, key):
     x = json.loads(x.text)
     x = x['list']
     i: int = 0
-    tab = ""
+    circleCity = []
     while i < cnt:
-        y = x[i]
-        main = y['main']
-        # date = y['dt_txt']
-        name = y['name']
-        temp = main['temp'] - 273.15
-        temp = round(temp)
-        temp_min = main['temp_min'] - 273.15
-        temp_min = round(temp_min)
-        temp_max = main['temp_max'] - 273.15
-        temp_max = round(temp_max)
-        tab = tab + "Ville: %s \ntemperature : %s c\ntemperature min" \
-                    " : %s c\ntemperature max : %s c\n" \
-                    % (name, temp, temp_min, temp_max)
-        i = i + 1
-        copie(tab)
+        y=x[i]
+        print(y['name'])
+        circleCity.append(y['name'])
+        i = i +1
+    return circleCity
 
-
-def meteo(city, key):
+def currentCoord(city, key):
     # requete post
+    url = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s" % (city, key)
+    x = requests.get(url)
+    x = json.loads(x.text)
+    # Recup coordonnée
+    x = x['coord']
+    lat = x['lat']
+    lon = x['lon']
+    coord = [lat, lon]
+    return coord
+
+
+def forecast(city, key):
+    # requete post
+    print(city)
     url = "https://api.openweathermap.org/data/2.5/forecast?lang=fr&q=%s&appid=%s" % (city, key)
     x = requests.get(url)
     try:
         x = json.loads(x.text)
         x = x['list']
-        print(x)
         i: int = 0
-        tab = ""
         while i < 40:
             y = x[i]
             main = y['main']
@@ -67,17 +69,22 @@ def meteo(city, key):
             temp_min = round(temp_min)
             temp_max = main['temp_max'] - 273.15
             temp_max = round(temp_max)
-            tab = tab + "Ville: %s  \ntemperature : %s c\ntemperature min" \
+            global tabCity
+            tabCity = tabCity + "Ville: %s  \ntemperature : %s c\ntemperature min" \
                         " : %s c\ntemperature max : %s c\ndate : %s \n" \
-                        % (city, temp, temp_min, temp_max, date)
+                      % (city, temp, temp_min, temp_max, date)
             i = i + 1
-        copie(tab)
     except Exception as e:
         print(e)
         newCity = input('entre une nouvelle ville avec le bon nom cette fois ... \n')
-        meteo(newCity, key)
+        forecast(newCity, key)
+    return multiCity
 
 
-# firstCity = input("Entrez la ville ")
-# meteo(firstCity,api_key)
-circle(attitude, longitude, api_key)
+firstCity = input("Entrez la ville ")
+coord = currentCoord(firstCity, api_key)
+multiCity = circle(coord[0], coord[1], api_key)
+
+for i in multiCity:
+    forecast(i, api_key)
+copie(tabCity)
